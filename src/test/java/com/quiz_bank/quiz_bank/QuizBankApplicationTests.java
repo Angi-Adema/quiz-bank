c2cd4234af692bd421ead2a86d3afd054e524cb8
Angi-Adema <dr.adema@gmail.com>
2024-11-11 21:32:02 -0700

    created all initial project files

---
+++ src/test/java/com/quiz_bank/quiz_bank/QuizBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz_bank.quiz_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
