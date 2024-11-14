59adee76a067ea059a9767545d08add027aa2c6f
Angi-Adema <dr.adema@gmail.com>
2024-11-14 11:54:29 -0700

    added logic to Quiz.java to establish attributes associated with each quiz.

--- src/main/java/com/quiz_bank/quiz_bank/quiz/Quiz.java
+++ src/main/java/com/quiz_bank/quiz_bank/quiz/Quiz.java
@@ -9,5 +9,40 @@ public class Quiz {
 	private String title;
 	private String description;
 	private List<Question> questions;
+	
+	// No argument constructor utilized for Hibernate to populate fields in the database, Spring to instantiate the object and Jackson to deserialize JSON data.
+	public Quiz() {
+		
+	}
 
+	public Quiz(String id, String title, String description, List<Question> questions) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.questions = questions;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public List<Question> getQuestions() {
+		return questions;
+	}
+
+	@Override
+	public String toString() {
+		return "Quiz [id=" + id + ", title=" + title + ", description=" + description + ", questions=" + questions
+				+ "]";
+	}
+ 
 }
