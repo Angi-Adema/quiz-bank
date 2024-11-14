bab153b803490797918fe5b92d2ff2642b11bb8f
Angi-Adema <dr.adema@gmail.com>
2024-11-14 15:52:31 -0700

    deleted the UserDetailsRepository.java and recreated it correcting the interface logic.

--- src/main/java/com/quiz_bank/quiz_bank/user/UserDetailsRepository.java
+++ src/main/java/com/quiz_bank/quiz_bank/user/UserDetailsRepository.java
@@ -4,11 +4,8 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class UserDetailsRepository {
+public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> {
 
-	public interface UserDetailsReporsitory extends JpaRepository<UserDetails, Long> {
-		
-		// Search users by role.
-		List<UserDetails> findByRole(String role);
-	}
+	// Search users by role.
+	List<UserDetails> findByRole(String role);
 }
