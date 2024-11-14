b4bf61102e9fe16961f466667cd600d77d285f39
Angi-Adema <dr.adema@gmail.com>
2024-11-14 12:22:20 -0700

    added the com.quiz_bank.quiz_bank.user package as well as UserDetails.java with logic to identify user attributes.

---
+++ src/main/java/com/quiz_bank/quiz_bank/user/UserDetails.java
@@ -0,0 +1,53 @@
+package com.quiz_bank.quiz_bank.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+// Manage by JPA so use @Entity.
+@Entity
+public class UserDetails {
+	
+	// Have Spring manage attributes of the user and enter @Id and @GeneratedValue.
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String role;
+	
+	// Populate user details in UserDetailsCommandLineRunner.java.
+	public UserDetails() {
+		
+	}
+
+	public UserDetails(Long id, String firstName, String lastName, String role) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDetails [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", role=" + role + "]";
+	}
+	
+}
